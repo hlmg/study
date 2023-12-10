c26fa7c314fece0fa6fd52558cc6f8c87f25bbb5
hlmg <devhlmg@gmail.com>
2023-12-11 00:46:01 +0900

    경계값 테스트 작성

--- practical-testing/src/test/java/sample/practicaltesting/unit/CafeKioskTest.java
+++ practical-testing/src/test/java/sample/practicaltesting/unit/CafeKioskTest.java
@@ -1,6 +1,7 @@
 package sample.practicaltesting.unit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 import sample.practicaltesting.unit.beverage.Americano;
@@ -19,6 +20,32 @@ class CafeKioskTest {
         // then
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // when
+        cafeKiosk.add(americano, 2);
+
+        // then
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> cafeKiosk.add(americano, 0))
+                .withMessage("음료는 1잔 이상 주문하실 수 있습니다.");
     }
 
     @Test
