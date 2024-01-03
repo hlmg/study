b527251386ec27acd1785d11b803249233d6bef5
hlmg <devhlmg@gmail.com>
2024-01-03 18:22:13 +0900

    API 호출: fetch, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import {Diary} from "./model/diary";
-import LifeCycle from "./LifeCycle";
+
+type FetchData = {
+  email: string;
+  body: string;
+};
 
 function App() {
 
   const [data, setData] = useState<Diary[]>([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res: FetchData[] = await fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json());
+
+    const initData = res.slice(0, 20)
+      .map(d => {
+        return {
+          author: d.email,
+          content: d.body,
+          emotion: Math.floor(Math.random() * 5) + 1,
+          created_date: new Date().getTime(),
+          id: dataId.current++,
+        }
+      });
+
+    setData(initData)
+  }
+
+  useEffect(() => {
+    getData()
+  }, []);
 
   const onEdit = ({id, content}: Pick<Diary, "id" | "content">) => {
     setData(data
@@ -36,7 +62,6 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle></LifeCycle>
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onDelete={onDelete} diaryList={data}/>
     </div>
